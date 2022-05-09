30f19ebafd06adee7d2369586a837602a5174cae
ivykolp <102769541+ivykolp@users.noreply.github.com>
2022-05-09 18:39:23 -0400

    updated script from code sandbox to match exactly

--- src/script.js
+++ src/script.js
@@ -23,38 +23,23 @@ if (minutes < 10) {
 let today = document.querySelector("#current-day");
 let time = document.querySelector("#current-time");
 
-function displayWeatherCondition(response) {
-  console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
-}
-
 today.innerHTML = day;
 time.innerHTML = `as of ${hours}:${minutes}`;
 
-function search(city) {
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+}
+
+function search(event) {
+  event.preventDefault();
   let apiKey = "14531cf5a5e3aad205f8ebad395806b8";
   let units = "metric";
+  let city = document.querySelector("#city-search").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function handleSubmit(event) {
-  event.preventDefault();
-
-  let city = document.querySelector("#city-search").value;
-  search(city);
-}
-
 let searchForm = document.querySelector("#city-form");
-searchForm.addEventListener("submit", handleSubmit);
+searchForm.addEventListener("submit", search);
 
 search("New York");
