da7b395624d664c1475a2bacad5b24457af83b61
ivykolp <102769541+ivykolp@users.noreply.github.com>
2022-05-12 18:52:04 -0400

    Updated date using timestamp function

--- src/script.js
+++ src/script.js
@@ -1,9 +1,39 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0:${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0:${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `Last updated as of ${day} ${hours}:${minutes}`;
+}
+
 function displayWeatherCondition(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  let dateElement = document.querySelector("#current-day");
+  console.log(dateElement);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -12,20 +42,18 @@ function displayWeatherCondition(response) {
     response.data.weather[0].main;
 }
 
-today.innerHTML = day;
-time.innerHTML = `as of ${hours}:${minutes}`;
-
-function search(city) {
+function search(currentCity) {
   let apiKey = "14531cf5a5e3aad205f8ebad395806b8";
   let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-search").value;
-  search(city);
+
+  let currentCity = document.querySelector("#city-search").value;
+  search(currentCity);
 }
 
 let searchForm = document.querySelector("#city-form");
