90f7e6b31e1e43c082c5e0a4e17150c0ec900b01
ivykolp <102769541+ivykolp@users.noreply.github.com>
2022-05-12 18:27:28 -0400

    Removed dates from JS
    going to replace "current time" with Updated as of ""

--- src/script.js
+++ src/script.js
@@ -1,28 +1,3 @@
-let now = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let today = document.querySelector("#current-day");
-let time = document.querySelector("#current-time");
-
 function displayWeatherCondition(response) {
   console.log(response.data);
   document.querySelector("#city").innerHTML = response.data.name;
